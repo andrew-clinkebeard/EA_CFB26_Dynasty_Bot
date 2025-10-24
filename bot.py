d57e8b2a5b162beb53d2d14e3edb799f05e03034
andrew-clinkenbeard <56001102+andrew-clinkenbeard@users.noreply.github.com>
2025-10-24 09:50:18 -0500

    updates to logic

--- bot.py
+++ bot.py
@@ -3,16 +3,12 @@
 #version
 __version__ = "0.0.1.3" 
 
-#commands string
-class msgCommands:
-    cmd = ""
-    GAME_RECAP_STR = "recap"
-    RECRUIT_SPOTLIGHT_STR = "recruit"
-    PRESS_RELEASE_STR = "press"
-    FAN_BLOG_STR = "fan"
-    RUMOR_MILL_STR = "rumor"
 
-VALID_CMD_STR = "Valid commands are !recap, !recruit, !press, and !fan"
+#constants
+VALID_CMD_STR = "Valid commands are !recap, !recruit, !press, !rumor, and !fan"
+
+#globals
+filePath = ""
 
 #packages
 import os
@@ -40,6 +36,37 @@ TARGET_CHANNEL_ID = 1431123905304596510  # example: 128502034523456789
 async def on_ready():
     print(f"{bot.user} is online and ready to receive DMs!")
 
+@bot.command()
+async def exit(ctx):
+    await ctx.send("Shutting down…")
+    await bot.close()
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.display_name}! 👋')
+
+@bot.command()
+async def recap(ctx):
+    global filePath
+    filePath = openAIClient.gameRecap()
+    
+@bot.command()
+async def recruit(ctx):
+    await ctx.send(f'recruit {ctx.author.display_name}! 👋')
+    
+@bot.command()
+async def press(ctx):
+    await ctx.send(f'press {ctx.author.display_name}! 👋')
+    
+@bot.command()
+async def fan(ctx):
+    await ctx.send(f'fan {ctx.author.display_name}! 👋')
+    
+@bot.command()
+async def rumor(ctx):
+    await ctx.send(f'rumor {ctx.author.display_name}! 👋')
+
+
 @bot.event
 async def on_message(message: discord.Message):
     # Ignore messages from the bot itself
@@ -50,45 +77,32 @@ async def on_message(message: discord.Message):
     if isinstance(message.channel, discord.DMChannel):
         #determine which message it is
         cmdIndex = message.content.find('!')
-        if cmdIndex != -1:
+        if cmdIndex == 0:
             #split command from rest of string
             msgParts = message.content.split(" " , 1)
-            msgCommands.cmd = msgParts[0].strip('!')
-            msgContents = msgParts[1]
 
-            match msgCommands.cmd:
-                case msgCommands.GAME_RECAP_STR:
-                    filePath = openAIClient.gameRecap()
-                case msgCommands.RECRUIT_SPOTLIGHT_STR:
-                    openAIClient.pressRelease()
-                case msgCommands.PRESS_RELEASE_STR:
-                    openAIClient.pressRelease()
-                case msgCommands.FAN_BLOG_STR:
-                    openAIClient.fanBlogs()
-                case _ :
-                    await message.channel.send(f"Invalid Command: {VALID_CMD_STR}")
-
-            guild = bot.get_guild(1431022749047984302)  # Replace with your server ID
-            if guild:
-                target_channel = guild.get_channel(TARGET_CHANNEL_ID)
-                if target_channel:
-                    # Post the DM content to your channel
-                    await target_channel.send(
-                        f"📩 DM from **{message.author}**: Command: {msgCommands.cmd} Contents: {msgContents}"
-                    )
-                     # Create a discord.File objectz
-                    file_to_send = discord.File(filePath)
-                    await target_channel.send(file=file_to_send, content="@everyone Hot off the press")
-                    #await target_channel.send(file=discord.File(r'C:\\Users\\Andrew Clinkenbeard\\Desktop\\8.jpg'))
-                else:
-                    print("Target channel not found.")
+            if len(msgParts) != 2:
+                await bot.process_commands(message) 
             else:
-                print("Guild not found.")
+                await bot.process_commands(message)
 
-            # Optionally reply to the user
-            await message.channel.send("Thanks! Your message has been sent to the admins. ✅")
+                guild = bot.get_guild(1431022749047984302)  # Replace with your server ID
+                if guild:
+                    target_channel = guild.get_channel(TARGET_CHANNEL_ID)
+                    if target_channel and len(filePath) != 0 :
+                        # Create a discord.File object
+                        file_to_send = discord.File(filePath)
+                        await target_channel.send(file=file_to_send, content=f"@everyone Hot off the press from {message.author.mention}")
+                        #await target_channel.send(file=discord.File(r'C:\\Users\\Andrew Clinkenbeard\\Desktop\\8.jpg'))
+                    else:
+                        print("Target channel not found.")
+                else:
+                    print("Guild not found.")
+
+                # Optionally reply to the user
+                await message.channel.send("Thanks! Your message has been sent to the admins. ✅")
         else:
-            print("Command not found.")
+            print(f"Invalid Command by {message.author}")
             #respond to dm with invalid cmd name and list valid cmd names
             await message.channel.send("Commands must start with !. {VALID_CMD_STR}")
 
