4c883f3a54679f5e6f306c5f035ef1f7041b0e82
andrew-clinkenbeard <56001102+andrew-clinkenbeard@users.noreply.github.com>
2025-11-09 18:36:07 -0600

    initail recruit command work, pushing for testing elsewhere

--- openAIClient.py
+++ openAIClient.py
@@ -1,11 +1,14 @@
 import os
 from openai import AsyncOpenAI
+from openai import OpenAI
+from dotenv import load_dotenv
+import requests
 from newspaper_recap import create_newspaper_pdf
 
 async def gameRecap(world_guide, style_guide, persona_style, game_input, reportName):
     client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     print("AI Opened")
-    prompt = f"{style_guide} {world_guide} {persona_style} {game_input}"
+    prompt = f"{style_guide}\nWolrd Info:\n{world_guide}\nWriter Style:\n{persona_style}\nGame Info:\n{game_input}"
     
     messages = [
     {"role": "system", "content": "You are a sports journalist creating recaps for College Football 26."},
@@ -29,10 +32,10 @@ async def gameRecap(world_guide, style_guide, persona_style, game_input, reportN
     filePath = create_newspaper_pdf("The Dynasty Tribune", subTitle, "Carter Langford", article, reportName)
     return filePath
 
-async def gamePreview(world_guide, style_guide, persona_style, userInput, reportName):
+async def gamePreview(world_guide, style_guide, persona_style, user_input, report_name):
     client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     print("AI Opened")
-    prompt = f"{style_guide} {world_guide} {persona_style} {userInput}"
+    prompt = f"{style_guide}\nWolrd Info:\n{world_guide}\nWriter Style:\n{persona_style}\nGame Info:\n{user_input}"
     
     messages = [
     {"role": "system", "content": "You are a sports journalist creating game previews for a game in College Football 26."},
@@ -53,17 +56,38 @@ async def gamePreview(world_guide, style_guide, persona_style, userInput, report
     print("subtitle Done")
 
     await client.close() 
-    filePath = create_newspaper_pdf("The Dynasty Tribune", subTitle, "RG3", article, reportName)
+    filePath = create_newspaper_pdf("The Dynasty Tribune", subTitle, "RG3", article, report_name)
     return filePath
 
-# def recruitSpotlight():
-#     print("recruit")
+def recruit_spotlight(style_guide, user_input, report_name):
+    
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    prompt = f"{style_guide}\n{user_input}"
+    result = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024"
+    )
 
-# def pressRelease():
-#     print("press")
+    image_url = result.data[0].url
+    response = requests.get(image_url)
 
-# def fanBlogs()
-#     print("fanBlogs")
+    with open(report_name, "wb") as f:
+        f.write(response.content)
+             
+    client.close() 
 
-# def rumorMill():
-#     print("rumor")
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Image Start")
+    prompt = "“high school football linebacker named Marcus ‘Tank’ Johnson, 6'3”, 230 lbs, 5 star from Travis High School. ”"
+    styleguid = """Creatoe a realistic carton picture with soft colors. Action shot of player in uniform with 
+    opponents visable if necessary. Setting is in a high school football stadium. Weather is location appropriate. Stadium lights and crowd in background. Slight depth blur for cinematic focus
+    High detail, dramatic lighting. Avoid any content that may be considered inappropriate or offensive, ensuring the image aligns with content policies"""
+    recruit_spotlight(
+        style_guide=styleguid,
+        user_input=prompt,
+        report_name=os.getcwd() + "\\test.png"
+    )
+    print("Image Done")
